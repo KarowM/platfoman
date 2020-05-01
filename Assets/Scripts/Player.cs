74033894ee9914b263fb3a760fd40db82c277dac
karow <karow.foxx@gmail.com>
2020-05-01 22:01:25 +0100

    - added input assets - player moves horizontally with wasd and arrow keys

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] int playerVelocity = 4;
+
+    Rigidbody2D playerRigidBody;
+    void Start() {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        Run();
+    }
+
+    private void Run() {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        playerRigidBody.velocity = new Vector2(controlThrow*playerVelocity, playerRigidBody.velocity.y);
+    }
+}
