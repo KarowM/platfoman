c091f384c5d2dd3e69d378fe669dfc60e5dc73f7
karow <karow.foxx@gmail.com>
2020-05-02 14:21:48 +0100

    - player faces directino of travel

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -14,10 +15,18 @@ public class Player : MonoBehaviour {
 
     void Update() {
         Run();
+        FlipPlayerHorizontally();
     }
 
     private void Run() {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         playerRigidBody.velocity = new Vector2(controlThrow*playerVelocity, playerRigidBody.velocity.y);
+    }
+
+    private void FlipPlayerHorizontally() {
+        if (Mathf.Abs(playerVelocity) > Mathf.Epsilon) {
+            Transform transform = GetComponent<Transform>();
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1);
+        }
     }
 }
