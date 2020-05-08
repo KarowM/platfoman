6b769912db15e1e51a968b529684d8c18bf4acd3
karow <karow.foxx@gmail.com>
2020-05-08 20:59:58 +0100

    - added enemy sprite sheet and sliced them - created enemy with walking animation - enemy has initial velocity of 1

---
+++ Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] int walkSpeed = 1;
+
+    Rigidbody2D enemyRigidbody;
+    void Start() {
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        enemyRigidbody.velocity = new Vector2(walkSpeed, 0);
+    }
+
+    void Update() {
+        FlipPlayerHorizontally();
+    }
+
+    private void FlipPlayerHorizontally() {
+        transform.localScale = new Vector2(Mathf.Sign(enemyRigidbody.velocity.x), 1);
+    }
+}
