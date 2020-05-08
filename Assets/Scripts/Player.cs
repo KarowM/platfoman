c0519be30d62dece9bd98e511a15bb6feb5ca5a4
karow <karow.foxx@gmail.com>
2020-05-08 11:48:36 +0100

    - added ladder sprite and tile - player climbs ladder with w key - player changes animation on climbing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private int playerVelocity = 4;
     [SerializeField] private int jumpVelocity = 16;
-
+    [SerializeField] private int climbVelocity = 5;
 
     Rigidbody2D playerRigidBody;
     Collider2D playerCollider;
@@ -21,14 +21,26 @@ public class Player : MonoBehaviour {
     }
 
     void Update() {
-        print(playerRigidBody.velocity.y);
         Run();
         FlipPlayerHorizontally();
         Jump();
+        Climb();
+        print(playerRigidBody.velocity);
+
+    }
+
+    private void Climb() {
+        bool playerIsTouchingLadder = playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"));
+
+        if (playerIsTouchingLadder) {
+            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, controlThrow * climbVelocity);
+            playerAnimator.SetBool("Climbing", PlayerIsMovingVertically());
+        }
     }
 
     private void Run() {
-        playerAnimator.SetBool("Running", PlayerIsMoving());
+        playerAnimator.SetBool("Running", PlayerIsMovingHorizontally());
 
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         playerRigidBody.velocity = new Vector2(controlThrow * playerVelocity, playerRigidBody.velocity.y);
@@ -41,7 +53,7 @@ public class Player : MonoBehaviour {
     }
 
     private void FlipPlayerHorizontally() {
-        if (PlayerIsMoving()) {
+        if (PlayerIsMovingHorizontally()) {
             transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1);
         }
     }
@@ -49,7 +61,11 @@ public class Player : MonoBehaviour {
         return playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
     }
 
-    private bool PlayerIsMoving() {
+    private bool PlayerIsMovingHorizontally() {
         return Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private bool PlayerIsMovingVertically() {
+        return Mathf.Abs(playerRigidBody.velocity.y) > Mathf.Epsilon;
     }
 }
