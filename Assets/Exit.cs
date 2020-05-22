264c768c754a5a7685d37bccbf9663c7202eb72f
karow <karow.foxx@gmail.com>
2020-05-22 17:21:43 +0100

    - put scenes into build - adding cave for progressing to next level

---
+++ Assets/Exit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        StartCoroutine(LoadNextLevel());
+        LoadNextLevel();
+    }
+
+    private static IEnumerator LoadNextLevel() {
+        yield return new WaitForSeconds(0.2f);
+
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
